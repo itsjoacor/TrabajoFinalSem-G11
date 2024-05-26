89207278b37f4ea53858597d7ec6b0797f814914
itsjoacor <jdr123@gmail.com>
2024-05-26 19:08:39 -0300

    Sem-Class creada

---
+++ src/SistemaDeEstacionamientoMedido.java
@@ -0,0 +1,39 @@
+
+public class SistemaDeEstacionamientoMedido {
+
+	public SistemaDeEstacionamientoMedido() {
+		
+	}
+	
+	public void cargarCelular(Int numero, double monto) {
+		
+	}
+	public void registrarTicket(Ticket ticket) {
+		
+	}
+	public void registarEstacionamiento(Estacionamiento e) {
+		
+	}
+	public void registrarUsuario(Usuario u) {
+		
+	}
+	public void registrarInfraccion(Infraccion i) {
+		
+	}
+	public void finalizarEstacionamiento(String telefono) {
+		
+	}
+	public void finalizarTodosLosEstacionamientos() {
+		
+	}
+	public boolean seEncuentraVigente(String pantente) {
+		
+	}
+	public void subscribirNotificado(Notificable n) {
+		
+	}
+	public void desubscribirNotificado(Notificable n) {
+		
+	}
+	
+}
