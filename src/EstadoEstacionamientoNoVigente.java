bf925f21965aa922c8fb5cc8049985b014327275
Tomas Neira Guitera <tomasneira181@gmail.com>
2024-06-07 20:39:02 -0300

    logica del state de AplicacionUsuario

--- src/EstadoEstacionamientoNoVigente.java
+++ src/EstadoEstacionamientoNoVigente.java
@@ -1,5 +1,14 @@
 
 public class EstadoEstacionamientoNoVigente extends EstadoEstacionamiento {
 	
-
+	
+	public void walking(AplicacionUsuario aplicacionUsuario) {
+		
+		aplicacionUsuario.notificarPosibleInicioEstacionamiento();
+	}
+	
+	public void iniciarEstacionamiento(AplicacionUsuario aplicacionUsuario, String patente) {
+		
+		aplicacionUsuario.iniciarEstacionamientoSEM(patente);
+	}
 }
--- src/EstadoEstacionamientoVigente.java
+++ src/EstadoEstacionamientoVigente.java
@@ -1,5 +1,14 @@
 
 public class EstadoEstacionamientoVigente extends EstadoEstacionamiento {
 	
+	public void driving(AplicacionUsuario aplicacionUsuario) {
+		
+		aplicacionUsuario.notificarPosibleFinEstacionamiento();
+	}
 	
+	
+	public void finalizarEstacionamiento(AplicacionUsuario aplicacionUsuario) {
+		
+		aplicacionUsuario.finalizarEstacionamientoSEM();
+	}
 }
